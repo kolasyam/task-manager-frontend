0af10a174a646bf8dd19c8c83e3ec81e51a594b1
kolasyam <syammanikantakola426@gmail.com>
2025-05-18 11:03:58 +0530

    changes

--- src/pages/PrivateRoute.tsx
+++ src/pages/PrivateRoute.tsx
@@ -1,18 +1,37 @@
-// src/components/PrivateRoute.tsx
-import React from "react";
-import { Navigate } from "react-router-dom";
-// import toast, { Toaster } from "react-hot-toast";
+// // src/components/PrivateRoute.tsx
+// import React from "react";
+// import { Navigate } from "react-router-dom";
+// // import toast, { Toaster } from "react-hot-toast";
 
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+// const PrivateRoute = ({ children }: { children: ReactElement }) => {
+//   const token = localStorage.getItem("usertoken");
+//   //   toast.success("Please Login before going to dashboard");
+//   if (!token) {
+//     return (
+//       <div>
+//         {/* <Toaster /> */}
+//         <Navigate to="/login" replace />;
+//       </div>
+//     );
+//   }
+
+//   return children;
+// };
+
+// export default PrivateRoute;
+// src/components/PrivateRoute.tsx
+import React, { ReactElement } from "react";
+import { Navigate } from "react-router-dom";
+
+interface PrivateRouteProps {
+  children: ReactElement;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const token = localStorage.getItem("usertoken");
-  //   toast.success("Please Login before going to dashboard");
+
   if (!token) {
-    return (
-      <div>
-        {/* <Toaster /> */}
-        <Navigate to="/login" replace />;
-      </div>
-    );
+    return <Navigate to="/login" replace />;
   }
 
   return children;
